468e844438605d6b889fe85961ac2181d4eccd32
Carlos Rodríguez Domínguez <carlos@everyware.es>
2018-01-26 17:09:28 +0100

    Initial Commit

---
+++ XML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XML
+//
+//  Created by Carlos Rodríguez Domínguez on 26/1/18.
+//  Copyright © 2018 Everyware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
